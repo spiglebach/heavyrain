56cb0ccce33ff431ffe502a9d3a4447a2f423c0a
Marcell Szukács <marcell.szukacs@r-szoft.hu>
2021-05-24 19:20:19 +0200

    randomized spawn positions

--- Assets/Scripts/Rainer.cs
+++ Assets/Scripts/Rainer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Rainer : MonoBehaviour {
@@ -16,22 +17,23 @@ public class Rainer : MonoBehaviour {
     [Header("Spawning parameters")]
     [SerializeField] private float minSpawnCooldownInSeconds = 1f;
     [SerializeField] private float maxSpawnCooldownInSeconds = 3f;
+    [SerializeField] private float spawnHeight = 5f;
     
     private float timeUntilNextSpawn = 2.5f;
+    private HashSet<Vector3> spawnPositions;
     
     void Start() {
         int xSize = maxX - minX + 1;
         int zSize = maxZ - minZ + 1;
-        bool[,] blocks = new bool[xSize, zSize]; // todo use list to ease random position assignment
-        Debug.Log("[" +blocks.GetLength(0) + "," + blocks.GetLength(1) + "]");
+        spawnPositions = new HashSet<Vector3>();
         for (int x = minX; x <= maxX; x++) {
             for (int z = minZ; z <= maxZ; z++) {
                 Vector3 rayStart = new Vector3(x, 1, z);
                 Vector3 down = Vector3.down;
                 RaycastHit hit;
                 if (Physics.Raycast(rayStart, down, out hit, 2, LayerMask.GetMask("Platforms"))) {
-                    blocks[xSize - maxX + x - 1, zSize - maxZ + z - 1] = true;
-                    Debug.Log("something found at: " + (rayStart + down).ToString());
+                    spawnPositions.Add(new Vector3(x, spawnHeight, z));
+                    Debug.Log("something found at: " + rayStart);
                     hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
                 }
             }
@@ -50,6 +52,9 @@ public class Rainer : MonoBehaviour {
         if (treasurePrefabs == null || treasurePrefabs.Length <= 0) {
             return;
         }
-        Instantiate(treasurePrefabs[0], new Vector3(0, 5, 0), Random.rotation); // todo assign random position on grid
+        Instantiate(
+            treasurePrefabs[0], // todo assign random prefab
+            spawnPositions.ElementAt(Random.Range(0, spawnPositions.Count)),
+            Random.rotation);
     }
 }
